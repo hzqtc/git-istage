2696efe94579633177d702a8ac573b321b99db41
Johnny Huang <hzqtc1229@gmail.com>
2025-06-16 22:14:31 -0700

    Removing diff mode.

--- README.md
+++ README.md
@@ -24,22 +24,6 @@ make install
 This will build and install the git-stage binary to ~/.local/bin.
 Ensure that ~/.local/bin is in your $PATH.
 
-Add a git alias 'git d' that would be used to show diff.
-[`difftasic`](https://github.com/Wilfred/difftastic) is recommended.
-For example, add the following lines to `~/.gitconfig`:
-
-```gitconfig
-[diff]
-  external = difft
-  tool = difftastic
-[difftool]
-  prompt = false
-[difftool "difftastic"]
-  cmd = difft --display side-by-side-show-both --color always "$MERGED" "$LOCAL" "abcdef1" "100644" "$REMOTE" "abcdef2" "100644"
-[alias]
-  d = difftool
-```
-
 ## 🚀 Usage
 
 Inside a Git repository:
@@ -50,10 +34,5 @@ git istage
 
 - ↑/↓ – navigate files
 - space – stage/unstage selected file
-- d - view diff (both staged/unstaged change vs HEAD)
-  - ↑/↓ - scroll by 1 line
-  - PgUp/PgDown - scroll by half screen
-  - g - scroll to top
-  - G - scroll to bottom
 - q or Ctrl+C – quit
 
--- main.go
+++ main.go
@@ -31,10 +31,6 @@ type model struct {
   files           []fileEntry
   cursor          int
   quitting        bool
-  diffMode        bool
-  diffContent     string
-  scrollOffset    int
-  viewportHeight  int
 }
 
 var (
@@ -68,26 +64,6 @@ func interpretGitStatus(xy string, filename string) (StagingStatus, *exec.Cmd, *
   }
 }
 
-func getDiff(f *fileEntry) string {
-  var diffCmd *exec.Cmd
-  switch f.status {
-  case Staged:
-    // Diff between staged vs HEAD
-    diffCmd =  exec.Command("git", "d", "--staged", f.name)
-  case Unstaged:
-    // Diff between unstaged vs HEAD
-    diffCmd =  exec.Command("git", "d", f.name)
-  case PartiallyStaged:
-    // Diff between working dir vs HEAD
-    diffCmd =  exec.Command("git", "d", "HEAD", f.name)
-  }
-  out, err := diffCmd.CombinedOutput()
-  if err != nil {
-    return fmt.Sprintf("Failed to show diff: %v", err)
-  }
-  return string(out)
-}
-
 func getGitChanges() ([]fileEntry, error) {
   // Check if we are in a git repository
   checkCmd := exec.Command("git", "rev-parse", "--is-inside-work-tree")
@@ -110,35 +86,12 @@ func getGitChanges() ([]fileEntry, error) {
       continue
     }
     // The first 2 letters on each line of `git status --porcelain` output represent status
-    status := line[:2]
+    xy := line[:2]
     filename := strings.TrimSpace(line[3:])
-    stagingStatus, stageCmd, unstageCmd := interpretGitStatus(status, filename)
-    files = append(files, fileEntry{name: filename, status: stagingStatus, stageCmd: stageCmd, unstageCmd: unstageCmd})
+    status, stageCmd, unstageCmd := interpretGitStatus(xy, filename)
+    files = append(files, fileEntry{name: filename, status: status, stageCmd: stageCmd, unstageCmd: unstageCmd})
   }
   return files, nil
-}
-
-func getMaxScroll(m *model) int {
-  lines := len(strings.Split(m.diffContent, "\n"))
-  if lines > m.viewportHeight {
-    return lines - m.viewportHeight
-  } else {
-    return 0
-  }
-}
-
-func moveCursorUp(m *model) {
-  if m.cursor > 0 {
-    m.cursor--
-    m.scrollOffset = 0
-  }
-}
-
-func moveCursorDown(m *model) {
-  if m.cursor < len(m.files) - 1 {
-    m.cursor++
-    m.scrollOffset = 0
-  }
 }
 
 func (m model) Init() tea.Cmd {
@@ -147,57 +100,20 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
   switch msg := msg.(type) {
-  case tea.WindowSizeMsg:
-    m.viewportHeight = msg.Height - len(m.files) - 1
-    return m, nil
   case tea.KeyMsg:
     switch msg.String() {
     case "ctrl+c", "q":
       m.quitting = true
       return m, tea.Quit
     case "up":
-      if m.diffMode {
-        if m.scrollOffset > 0 {
-          m.scrollOffset--
-        } else {
-          // Go to previous file if at top of scroll
-          moveCursorUp(&m)
-        }
-      } else {
-        moveCursorUp(&m)
+      if m.cursor > 0 {
+        m.cursor--
       }
     case "down":
-      if m.diffMode {
-        if m.scrollOffset < getMaxScroll(&m) {
-          m.scrollOffset++
-        } else {
-          // Go to next file if at top of scroll
-          moveCursorDown(&m)
-        }
-      } else {
-        moveCursorDown(&m)
-      }
-    case "pgdown":
-      if m.diffMode {
-        maxScroll := getMaxScroll(&m)
-        newScrollOffset := m.scrollOffset + m.viewportHeight / 2
-        if newScrollOffset > maxScroll {
-          newScrollOffset = maxScroll
-        }
-        m.scrollOffset = newScrollOffset
-      }
-    case "pgup":
-      if m.diffMode {
-        newScrollOffset := m.scrollOffset - m.viewportHeight / 2
-        if newScrollOffset < 0 {
-          newScrollOffset = 0
-        }
-        m.scrollOffset = newScrollOffset
+      if m.cursor < len(m.files) - 1 {
+        m.cursor++
       }
     case " ":
-      if m.diffMode {
-        break;
-      }
       f := &m.files[m.cursor]
       switch f.status {
       case Staged:
@@ -207,19 +123,6 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
         f.stageCmd.Run()
         f.status = Staged
       }
-    case "d":
-      m.diffMode = !m.diffMode
-    case "g":
-      if m.diffMode {
-        m.scrollOffset = 0
-      }
-    case "G":
-      if m.diffMode {
-        m.scrollOffset = getMaxScroll(&m)
-      }
-    }
-    if m.diffMode {
-      m.diffContent = getDiff(&m.files[m.cursor])
     }
   }
   return m, nil
@@ -231,26 +134,6 @@ func (m model) View() string {
   }
 
   var b strings.Builder
-
-  if m.diffMode {
-    lines := strings.Split(m.diffContent, "\n")
-
-    // Slice visible lines
-    end := m.scrollOffset + m.viewportHeight
-    if end > len(lines) {
-      end = len(lines)
-    }
-    visibleLines := lines[m.scrollOffset:end]
-
-    b.WriteString(strings.Join(visibleLines, "\n"))
-    b.WriteString(fmt.Sprintf(
-      "\n↑/↓/PageUp/PageDown scroll (%d/%d)  g: top  G: bottom  d: back  q: quit\n",
-      end,
-      len(lines),
-    ))
-    return b.String()
-  }
-
   for i, f := range m.files {
     var cursor string
     if i == m.cursor {
@@ -270,7 +153,7 @@ func (m model) View() string {
     b.WriteString(fmt.Sprintf("%s%s %s\n", cursor, checkbox, f.name))
   }
 
-  b.WriteString("\n↑/↓: navigate  space: toggle  d: diff  q: quit\n")
+  b.WriteString("\n↑/↓: navigate  space: toggle  q: quit\n")
   return b.String()
 }
 

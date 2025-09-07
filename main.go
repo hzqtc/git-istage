a0c8e97d8e7999c0fc575cd888a8d437e61a6556
Johnny Huang <hzqtc1229@gmail.com>
2025-09-06 19:15:36 -0700

    Show diff stats for each file

--- main.go
+++ main.go
@@ -5,24 +5,37 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 )
 
-type StagingStatus int
+type stagingStatus int
 
 const (
-	Unstaged StagingStatus = iota
-	Staged
-	PartiallyStaged
+	unstaged stagingStatus = iota
+	staged
+	partiallyStaged
 )
 
 type fileEntry struct {
 	pathFromGitRoot string
 	pathFromCwd     string
-	status          StagingStatus
+	status          stagingStatus
+	diff            diffStat
+}
+
+type diffStat struct {
+	added   int
+	deleted int
+}
+
+func (d diffStat) combine(o diffStat) diffStat {
+	d.added += o.added
+	d.deleted += o.deleted
+	return d
 }
 
 type model struct {
@@ -37,28 +50,39 @@ var (
 	unstagedStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
 )
 
-func interpretGitStatus(xy string) StagingStatus {
+var gitRootPath = func() string {
+	gitRootCmd := exec.Command("git", "rev-parse", "--show-toplevel")
+	gitRootBytes, _ := gitRootCmd.Output()
+	return strings.TrimSpace(string(gitRootBytes))
+}()
+
+var currentPath = func() string {
+	cwd, _ := os.Getwd()
+	return cwd
+}()
+
+func interpretGitStatus(xy string) stagingStatus {
 	x, y := xy[0], xy[1]
 
 	switch {
 	case x == '?' && y == '?':
 		// Cover cases: '??'
-		return Unstaged
+		return unstaged
 	case x == 'A' && y != ' ':
 		// Cover cases: 'AM'
-		return PartiallyStaged
+		return partiallyStaged
 	case x != ' ' && y != ' ':
 		// Cover cases: '*M'
-		return PartiallyStaged
+		return partiallyStaged
 	case x == 'A':
 		// Cover cases: 'A '
-		return Staged
+		return staged
 	case x != ' ':
 		// Cover cases: '* '
-		return Staged
+		return staged
 	default:
 		// Cover cases: ' *'
-		return Unstaged
+		return unstaged
 	}
 }
 
@@ -70,36 +94,106 @@ func getGitChanges() ([]fileEntry, error) {
 		return nil, fmt.Errorf("Not inside a git repository")
 	}
 
-	// Get porcelain status
-	statusCmd := exec.Command("git", "status", "--porcelain")
-	out, err := statusCmd.Output()
-	if err != nil {
-		return nil, err
-	}
-
-	// Get the git root
-	gitRootCmd := exec.Command("git", "rev-parse", "--show-toplevel")
-	gitRootBytes, _ := gitRootCmd.Output()
-	gitRoot := strings.TrimSpace(string(gitRootBytes))
-	// Get cwd
-	cwd, _ := os.Getwd()
+	statusCh := make(chan map[string]stagingStatus)
+	diffStatsCh := make(chan map[string]diffStat)
+	go func() {
+		statusCh <- getFileStatus()
+	}()
+	go func() {
+		diffStatsCh <- getFileDiffStats()
+	}()
+	status := <-statusCh
+	diffStats := <-diffStatsCh
 
 	var files []fileEntry
-	for line := range strings.SplitSeq(string(out), "\n") {
+	for path, st := range status {
+		files = append(files,
+			fileEntry{
+				pathFromGitRoot: path,
+				pathFromCwd:     getRelPath(path),
+				status:          st,
+				diff:            diffStats[path],
+			})
+	}
+	return files, nil
+}
+
+func getFileStatus() map[string]stagingStatus {
+	result := make(map[string]stagingStatus)
+	// Get porcelain status
+	cmd := exec.Command("git", "status", "--porcelain")
+	output, err := cmd.Output()
+	if err != nil {
+		return result
+	}
+
+	for line := range strings.SplitSeq(string(output), "\n") {
 		if len(line) < 4 {
 			continue
 		}
 		// The first 2 letters on each line of `git status --porcelain` output represent status
 		xy := line[:2]
-		status := interpretGitStatus(xy)
-		// `git status --porcelain` always output file path relative to git root
-		pathFromGitRoot := strings.TrimSpace(line[3:])
-		// So we need to convert it to relative path to CWD
-		absPath := filepath.Join(gitRoot, pathFromGitRoot)
-		pathFromCwd, _ := filepath.Rel(cwd, absPath)
-		files = append(files, fileEntry{pathFromGitRoot, pathFromCwd, status})
+		pathFromGitRoot := line[3:]
+		result[pathFromGitRoot] = interpretGitStatus(xy)
 	}
-	return files, nil
+	return result
+}
+
+func getFileDiffStats() map[string]diffStat {
+	result := make(map[string]diffStat)
+	diffCh := make(chan map[string]diffStat)
+	const diffCmdNum = 2
+
+	go func() {
+		cmd := exec.Command("git", "diff", "--numstat")
+		output, err := cmd.Output()
+		if err != nil {
+			diffCh <- nil
+		}
+		diffCh <- parseDiffOutput(string(output))
+	}()
+
+	go func() {
+		cmd := exec.Command("git", "diff", "--numstat", "--cached")
+		output, err := cmd.Output()
+		if err != nil {
+			diffCh <- nil
+		}
+		diffCh <- parseDiffOutput(string(output))
+	}()
+
+	for range diffCmdNum {
+		diff := <-diffCh
+		for path, d := range diff {
+			result[path] = result[path].combine(d)
+		}
+	}
+
+	return result
+}
+
+func parseDiffOutput(output string) map[string]diffStat {
+	result := make(map[string]diffStat)
+	for line := range strings.SplitSeq(output, "\n") {
+		parts := strings.Fields(line)
+		if len(parts) < 3 {
+			continue
+		}
+		added, _ := strconv.Atoi(parts[0])
+		deleted, _ := strconv.Atoi(parts[1])
+		pathFromGitRoot := parts[2]
+		result[pathFromGitRoot] = diffStat{added, deleted}
+	}
+	return result
+}
+
+// Convert a path from git root to a relative path of cwd
+func getRelPath(pathFromGitRoot string) string {
+	// many git commands output file path relative to git root
+	// So we need to convert it to relative path to CWD
+	absPath := filepath.Join(gitRootPath, pathFromGitRoot)
+	pathFromCwd, _ := filepath.Rel(currentPath, absPath)
+	return pathFromCwd
 }
 
 func (m model) Init() tea.Cmd {
@@ -149,18 +243,25 @@ func (m *model) cursorDown() {
 func (m *model) toggle(index int) {
 	f := &m.files[index]
 	switch f.status {
-	case Staged:
+	case staged:
 		exec.Command("git", "restore", "--staged", f.pathFromCwd).Run()
-		f.status = Unstaged
-	case PartiallyStaged, Unstaged:
+		f.status = unstaged
+	case partiallyStaged, unstaged:
 		exec.Command("git", "add", f.pathFromCwd).Run()
-		f.status = Staged
+		f.status = staged
 	}
 }
 
 func (m model) View() string {
 	if m.quitting {
 		return ""
+	}
+
+	maxFilenameLen := 0
+	maxAddedLen := 0
+	for _, f := range m.files {
+		maxFilenameLen = max(maxFilenameLen, len(f.pathFromGitRoot))
+		maxAddedLen = max(maxAddedLen, len(strconv.Itoa(f.diff.added)))
 	}
 
 	var b strings.Builder
@@ -173,14 +274,23 @@ func (m model) View() string {
 		}
 		var checkbox string
 		switch f.status {
-		case Staged:
+		case staged:
 			checkbox = stagedStyle.Render("[✓]")
-		case PartiallyStaged:
+		case partiallyStaged:
 			checkbox = unstagedStyle.Render("[~]")
-		case Unstaged:
+		case unstaged:
 			checkbox = unstagedStyle.Render("[ ]")
 		}
-		b.WriteString(fmt.Sprintf("%s%s %s\n", cursor, checkbox, f.pathFromGitRoot))
+		b.WriteString(fmt.Sprintf(
+			"%s%s %s%s %s+%d/-%d\n",
+			cursor,
+			checkbox,
+			f.pathFromGitRoot,
+			strings.Repeat(" ", maxFilenameLen-len(f.pathFromGitRoot)),
+			strings.Repeat(" ", maxAddedLen-len(strconv.Itoa(f.diff.added))),
+			f.diff.added,
+			f.diff.deleted,
+		))
 	}
 
 	b.WriteString("\nj/k/↑/↓: navigate | space: toggle | a: toggle all | q: quit\n")
